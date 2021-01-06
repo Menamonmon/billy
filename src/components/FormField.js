6c8ac5a839eed367b1706a32e28c69a9f37d9614
Menamonmon <monyasser111@gmail.com>
2021-01-05 21:41:18 -0500

    Form field now recieves props with ...rest

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -2,18 +2,16 @@ import React from "react";
 import { FormControl, InputLabel, OutlinedInput } from "@material-ui/core";
 import "./FormField.css";
 
-export default function FormField({ value, type, label, name, onChange }) {
+export default function FormField(props) {
+  const { label, ...rest } = props;
   const labelId = label.toLowerCase();
   return (
     <FormControl style={{ margin: "20px 0" }} variant="outlined">
       <InputLabel htmlFor={labelId}>{label}</InputLabel>
       <OutlinedInput
         id={labelId}
-        name={name}
-        value={value}
-        onChange={onChange}
         label={label}
-        type={type}
+        {...rest}
       />
     </FormControl>
   );
